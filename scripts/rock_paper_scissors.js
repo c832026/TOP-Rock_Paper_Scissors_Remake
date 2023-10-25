1ce491a7de0f7b10609d4b9758a18e76fbf5f665
c832026 <c832026@gmail.com>
2023-10-25 20:22:00 +0800

    Finish getPlayerSelection function

--- scripts/rock_paper_scissors.js
+++ scripts/rock_paper_scissors.js
@@ -3,7 +3,15 @@ function getComputerChoice() {
     let computerChoice = Math.floor((Math.random() * 100)) % 3;
 
     // Return choice message determined by generated integer (0-2)
-    return computerChoice === 0 ? "Rock" 
-         : computerChoice === 1 ? "Paper" 
-         : "Scissors"
+    return computerChoice === 0 ? "rock" 
+         : computerChoice === 1 ? "paper" 
+         : "scissors"
 }
+
+function getPlayerSelection() {
+    let playerSelection = prompt("Make a play!(Rock, Paper, Scissors)");
+    playerSelection = playerSelection.toLowerCase();
+    return playerSelection;
+}
+
+const playerSelection = getPlayerSelection();
